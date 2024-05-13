5611c211214597782111e1f56f3415c60f3e9c32
Hayeong <90609686+choihayeong@users.noreply.github.com>
2024-05-13 17:45:19 +0900

    setting for react study with vite

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./scss/App.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
