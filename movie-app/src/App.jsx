7ba8446827291ef51f7a9c13289a8f43386bfe09
Hayeong <90609686+choihayeong@users.noreply.github.com>
2024-05-22 14:27:31 +0900

    component props, useEffect, useRef

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./scss/App.scss";
 
 import Button from "./components/Button";
@@ -6,15 +6,29 @@ import Button from "./components/Button";
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    console.log("aaa");
+  }, []);
+
+  const buttonRef = useRef();
+
+  const inputRef = useRef();
+  const inputChangeTest = () => {
+    console.log(inputRef.current.value);
+  };
+
+
   return (
     <>
       <p>
         count : {count}
       </p>
-      
-      
-      <Button /> {/* 해당부분 추가 */}
-      <button type="button" onClick={() => setCount(cnt => cnt+1)}>Click</button>
+    
+      <Button btnRef={buttonRef} text="Click" buttonClickEvent={() => setCount(cnt => cnt+1)} /> {/* 해당부분 추가 */}
+
+      <hr />
+
+      <input type="text" ref={inputRef} onChange={inputChangeTest} placeholder="User Name" />
     </>
   );
 }
--- movie-app/src/components/Button.jsx
+++ movie-app/src/components/Button.jsx
@@ -1,5 +1,5 @@
 import styles from "../scss/Button.module.scss";
-
+import PropTypes from "prop-types";
 
 /**
  * function 컴포넌트이름() {
@@ -9,10 +9,23 @@ import styles from "../scss/Button.module.scss";
  * }
  */
 
-function Button() {
+function Button(props) {
+    const { text, btnRef, buttonClickEvent } = props;
+
     return (
-        <button type="button" className={styles.btn}>Default</button>
+        <button type="button" ref={btnRef} className={styles.btn} onClick={buttonClickEvent} >{text}</button>
     )
 }
 
 export default Button;
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    btnRef: PropTypes.oneOfType([
+        // Either a function
+        PropTypes.func, 
+        // Or the instance of a DOM native element (see the note about SSR)
+        PropTypes.shape({ current: PropTypes.instanceOf(Element) })
+    ]),
+    buttonClickEvent: PropTypes.func,
+};
